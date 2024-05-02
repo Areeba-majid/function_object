fad1184ce8f9194110fc230ddb622b8f10d6adde
Areeba Riaz <160718011+Areeba-majid@users.noreply.github.com>
2024-05-02 09:51:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var car = {
+    carName: "BMW",
+    model: 2024,
+    colour: "Black",
+    speed: "100km per hr",
+    capacity: 6,
+    automatic: true
+};
+console.log("my car name is ".concat(car.carName));
---
+++ main.ts
@@ -0,0 +1,10 @@
+let car = {
+    carName: "BMW",
+    model: 2024 ,
+    colour: "Black" ,
+    speed: "100km per hr",
+    capacity: 6,
+    automatic: true
+};
+
+console.log(`my car name is ${car.carName}`)
